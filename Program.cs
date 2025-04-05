f3c13323fefdab30bb90a165199928d3221aad44
Andrew D'Antonio <andrew.dantonio@gmail.com>
2025-04-04 22:55:11 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+
+namespace Tester
+{
+    internal class Program
+    {
+ 
+        static void Main(string[] args)
+        {
+            TcpServer tcpServer = new TcpServer();
+            tcpServer.OnConnected += TcpServerConnectedHandler;
+            tcpServer.OnDisconnected += TcpServerClientDisconnectedHandler;
+            tcpServer.Start(IPAddress.Any, 8080);
+            Console.WriteLine("Listening...");
+            Console.ReadKey(true);
+            Console.WriteLine("Stopping sever...");
+            tcpServer.Stop();
+        }
+
+        private static void TcpServerClientDisconnectedHandler(object? sender, EventArgs e)
+        {
+            Console.WriteLine("Client disconnected");
+        }
+
+        private static void TcpServerConnectedHandler(object? sender, EventArgs e)
+        {
+            Console.WriteLine("Client connected");
+        }
+    }
+}
---
+++ TcpServer.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Tester
+{
+    public class ErrorEventArgs : EventArgs
+    {
+        public string ErrorMessage { get; set; }
+        public ErrorEventArgs(string errorMessage)
+        {
+            ErrorMessage = errorMessage;
+        }
+    }
+
+    public class TcpServer
+    {
+        private Hashtable clientBuffer = [];
+        private CancellationTokenSource cancellationToken = new();
+
+        public event EventHandler OnConnected = delegate (object? sender, EventArgs e) { };
+        public event EventHandler OnDisconnected = delegate (object? sender, EventArgs e) { };
+        public event EventHandler OnError = delegate (object? sender, EventArgs e) { };
+
+        private void InitializeClientBuffer(TcpClient client)
+        {
+            try
+            {
+                nint key = client.Client.Handle;
+                if (!clientBuffer.ContainsKey(key))
+                {
+                    clientBuffer.Add(key, null);
+                }
+                else
+                {
+                    clientBuffer[key] = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                OnError?.Invoke(this, new ErrorEventArgs(ex.Message));
+                client.Close();
+            }
+        }
+
+        private async void ReceiveData(TcpClient client, CancellationToken cancellationToken)
+        {
+            try
+            {
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = new byte[1024];
+                while (client.Connected && !cancellationToken.IsCancellationRequested)
+                {
+                    int bytesRead = await stream.ReadAsync(buffer, cancellationToken);
+                    if (bytesRead != 0)
+                    {
+                        string message = System.Text.Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                        lock (clientBuffer)
+                        {
+                            if (clientBuffer[client.Client.Handle] is null)
+                            {
+                                clientBuffer[client.Client.Handle] = message;
+                            }
+                            else
+                            {
+                                clientBuffer[client.Client.Handle] += message;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                OnError?.Invoke(this, new ErrorEventArgs(ex.Message));
+                client.Close();
+            }
+            finally
+            {
+                OnDisconnected?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        private void RemoveClientBuffer(nint handle)
+        {
+            lock (clientBuffer)
+            {
+                if (clientBuffer.ContainsKey(handle))
+                {
+                    clientBuffer.Remove(handle);
+                }
+            }
+        }
+
+        private void HandleClient(TcpClient client, CancellationToken cancellationToken)
+        {
+            InitializeClientBuffer(client);
+            ReceiveData(client, cancellationToken);
+            RemoveClientBuffer(client.Client.Handle);
+        }
+
+        public void Start(IPAddress ipAddress, int port)
+        {
+            TcpListener listener = new(ipAddress, port);
+            listener.Start();
+            Task.Run(() =>
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    listener.AcceptTcpClientAsync(cancellationToken.Token).AsTask().ContinueWith((task) =>
+                    {
+                        if (task.IsCompletedSuccessfully)
+                        {
+                            TcpClient client = task.Result;
+
+                            OnConnected?.Invoke(this, EventArgs.Empty);
+                            HandleClient(client, cancellationToken.Token);
+                        }
+                    });
+                }
+            }, cancellationToken.Token);
+        }
+
+        public void Stop()
+        {
+            cancellationToken.Cancel();
+        }
+
+    }
+}
